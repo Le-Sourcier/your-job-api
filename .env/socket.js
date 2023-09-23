be7a368bbd569eb69044944d0a882cfd5a1e44d3
Le-Sourcier <hackersranch@gmail.com>
2023-09-23 07:49:59 +0000

    Your Job backend

--- .env/socket.js
+++ .env/socket.js
@@ -1,33 +1,33 @@
-const socketIo = require("socket.io");
+// const socketIo = require("socket.io");
 
-const http = require("http");
-const express = require("express");
-const app = express(http);
-const server = http.createServer(app);
+// const http = require("http");
+// const express = require("express");
+// const app = express(http);
+// const server = http.createServer(app);
 
-const route = express.Router();
+// const route = express.Router();
 
-const io = socketIo(server);
+// const io = socketIo(server);
 
-io.on("connection", (socket) => {
-  const successMessage = "User connected";
-  console.log(successMessage);
-  // socket.broadcast.emit("connected");
-  socket.emit("connection", successMessage);
+// io.on("connection", (socket) => {
+//   const successMessage = "User connected";
+//   console.log(successMessage);
+//   // socket.broadcast.emit("connected");
+//   socket.emit("connection", successMessage);
 
-  // socket.on("message", async (query) => {
-  //   Model.getUserData(query, socket).catch((error) => {
-  //     console.log(error);
-  //     socket.emit("message", error);
-  //   });
-  // });
+//   // socket.on("message", async (query) => {
+//   //   Model.getUserData(query, socket).catch((error) => {
+//   //     console.log(error);
+//   //     socket.emit("message", error);
+//   //   });
+//   // });
 
-  socket.on("disconnect", () => {
-    //socket.emit("disconnect");
-    const errorMessage = "User disconnected";
-    console.log(errorMessage);
-    // socket.emit("disconnect", errorMessage);
-  });
-});
+//   socket.on("disconnect", () => {
+//     //socket.emit("disconnect");
+//     const errorMessage = "User disconnected";
+//     console.log(errorMessage);
+//     // socket.emit("disconnect", errorMessage);
+//   });
+// });
 
-module.exports = route;
+// module.exports = route;
